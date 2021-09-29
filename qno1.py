04c922a528b4f08d306cad90998f17d4c043b137
NongArundas <nongkakrun@gmail.com>
2021-09-29 18:20:42 +0530

    update

---
+++ qno1.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the value of n: "))
+for i in range(1, n+1):
+    for j in range(1, i+1):
+            print("*", end = " ")
+    print(" ")
+for i in range(1, n+1):
+    for j in range(n, i, -1):
+        print("*", end = " ")
+    print(" ")
